c5a550abbb5b1b6f54b16497857b876137a3b9aa
Kanjina <pengsenfang99@gmail.com>
2026-05-17 15:44:10 -0700

    word_count.py

--- word_count.py
+++ word_count.py
@@ -2,20 +2,16 @@
 This is a simple python program which inputs two strings and detects whether the second string
 is within the first string. This program is case insensitive.
 """
-
-Text = input("Enter a string: ")
+Text = str(input("Enter a string: "))
 word = Text.lower()
-
-oo = input("Enter the word you want to find (case insensitive): ")
+oo = str(input("Enter the word you want to find (case insensitive): "))
 leng = len(oo)
 o = oo.lower()
-
 def word_count(o):
     count = 0
     for i in range(len(word) - leng + 1):
         w = word[i:i + leng]
         if w == o:
-            count += 1
+            count = count + 1
     return count
-
 print(word_count(o))
