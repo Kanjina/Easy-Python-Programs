0aa63ba33c5b7a69fe10fca5ca335adb0f32fbaf
Kanjina <pengsenfang99@gmail.com>
2026-05-17 15:39:37 -0700

    word_count.py
    This is a simple program that searches for the second string within the first string. It is case-insensitive.
    Note: Text = ooooo and oo = oo would return 4
    This is my first coding mini project

---
+++ word_count.py
@@ -0,0 +1,21 @@
+"""
+This is a simple python program which inputs two strings and detects whether the second string
+is within the first string. This program is case insensitive.
+"""
+
+Text = input("Enter a string: ")
+word = Text.lower()
+
+oo = input("Enter the word you want to find (case insensitive): ")
+leng = len(oo)
+o = oo.lower()
+
+def word_count(o):
+    count = 0
+    for i in range(len(word) - leng + 1):
+        w = word[i:i + leng]
+        if w == o:
+            count += 1
+    return count
+
+print(word_count(o))
